4bf8cd563637498dd1f02a5605950ff6773efc3d
Lucas P <lucas_wuthang@hotmail.com>
2020-02-12 23:28:39 -0300

    Criado controller para adicionar usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, resp) => {
-  const user = await User.create({
-    name: 'Lucas Pereira',
-    email: 'lucas@teste.com.br',
-    password_hash: '1478523',
-  });
-
-  return resp.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
