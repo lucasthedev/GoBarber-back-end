44c39c7ffeff88b4b80ade544654fb6e9c0a3c2b
Lucas Pereira <lucaspereira@air-de-lucas.localdomain>
2020-04-12 21:40:26 -0300

    Adicionando dependencia e implementação para rodar aplicação em MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -17,6 +19,17 @@ class Database {
     models
       .map(model => model.init(this.conncetion))
       .map(model => model.associate && model.associate(this.conncetion.models));
+  }
+
+  mongo() {
+    this.mongo.conncetion = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
